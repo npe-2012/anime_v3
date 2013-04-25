299ea391cb491f4f14e89ab1db13cd129539d716
Tsukino Usagi <usagi@taipei.usagi.tw>
2013-04-25 11:11:55 +0800

    grocery grid installation and basic setup

---
+++ application/controllers/anime.php
@@ -0,0 +1,47 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Anime extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('anime_model');
+        $this->load->helper('url');
+        $this->load->library('grocery_CRUD');
+
+    }
+	public function index()
+	{
+		$this->load->view('anime');
+	}
+
+    public function show() {
+        $this->grocery_crud->set_table('anime');
+        $output = $this->grocery_crud->render();
+
+        $this->_grid_output($output);
+    }
+
+    function _grid_output($output) {
+        $this->load->view('anime.php', $output);
+    }
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
